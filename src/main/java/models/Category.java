81da28e4cccb180feea8baf9684286e715086a1f
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-25 14:53:13 +0100

    Category enum created

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -1,4 +1,26 @@
 package models;
 
 public enum Category {
+
+    APPLIANCES("Appliances"),
+    AUDIOANDSTEREO("Audio and Stereo"),
+    KIDSTHINGS("Children"),
+    TECHNOLOGY("Technology"),
+    CHRISTMASDECORATIONS("Christmas Decorations"),
+    CLOTHESANDFOOTWEAR("Clothing and Footwear"),
+    DIY("DIY"),
+    HEALTHANDBEAUTY("Health and Beauty"),
+    HOMEANDGARDEN("Home and Garden"),
+    MUSICFILMSBOOKSGAMES("Music, Films, Books and Games"),
+    MUSICALINSTRUMENTS("Musical Instruments"),
+    OFFICEEQUIPMENT("Office Equipment"),
+    SPORTS("Sports"),
+    FREEBIES("Freebies"),
+    MISCELLANEOUS("Miscellaneous");
+
+    Category(String value) {
+    }
 }
+
+
+
--- src/test/java/AdvertTest.java
+++ src/test/java/AdvertTest.java
@@ -1,2 +1,14 @@
+import models.Advert;
+import org.junit.Before;
+
 public class AdvertTest {
+
+    Advert advert;
+
+    @Before
+
+    public void before() {
+
+        advert = new Advert("Nokia 3310", "Classic Nokia indestructable mobile phone for sale", )
+    }
 }
