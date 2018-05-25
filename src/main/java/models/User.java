471f238af7ddec40f78ab8cab86c016c55ab7315
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-25 15:06:51 +0100

    user class set up

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,62 @@
 package models;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private ArrayList<Advert> adverts;
+    private ArrayList<Comment> comments;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.adverts = new ArrayList<>();
+        this.comments = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Advert> getAdverts() {
+        return adverts;
+    }
+
+    public void setAdverts(ArrayList<Advert> adverts) {
+        this.adverts = adverts;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<Comment> comments) {
+        this.comments = comments;
+    }
 }
--- src/test/java/AdvertTest.java
+++ src/test/java/AdvertTest.java
@@ -1,14 +1,18 @@
 import models.Advert;
+import models.Category;
+import models.User;
 import org.junit.Before;
 
 public class AdvertTest {
 
     Advert advert;
+    User user;
 
     @Before
 
     public void before() {
 
-        advert = new Advert("Nokia 3310", "Classic Nokia indestructable mobile phone for sale", )
+        user = new User("Bob Smith", "bobsmith1@hotmail.com");
+        advert = new Advert("Nokia 3310", "Classic Nokia indestructable mobile phone for sale", Category.TECHNOLOGY, 50.00, "Glasgow", user, "testphoto.imgur.com");
     }
 }
