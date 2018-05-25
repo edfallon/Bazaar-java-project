5218d70a37fdf7df6103bfaea6e3f32f0015b0ab
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-25 15:22:57 +0100

    getter for comment title test passing

--- src/test/java/CommentTest.java
+++ src/test/java/CommentTest.java
@@ -3,6 +3,9 @@ import models.Category;
 import models.Comment;
 import models.User;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CommentTest {
 
@@ -17,4 +20,11 @@ public class CommentTest {
         advert = new Advert("Nokia 3310", "Classic Nokia indestructable mobile phone for sale", Category.TECHNOLOGY, 50.00, "Glasgow", user, "testphoto.imgur.com");
         comment = new Comment("Question about product", "Does product have any changeable covers?", user, advert);
     }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("Question about product", comment.getTitle());
+    }
+
+
 }
