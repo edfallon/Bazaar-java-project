e7781db88f323da0e4d7c9b7179a79c040e58439
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-25 15:46:43 +0100

    Added initial setup to main controller

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,4 +1,23 @@
 package controllers;
 
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
 public class MainController {
+
+    public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        get("/", (req, res) -> {
+            return "Hello, World!";
+        });
+
+    }
 }
