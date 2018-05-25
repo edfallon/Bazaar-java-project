a9b571e9f8d43f47870ec5c11930b4577b78c3a6
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-25 16:42:28 +0100

    seeds refactored

--- src/main/java/db/Seeds.java
+++ src/main/java/db/Seeds.java
@@ -1,6 +1,7 @@
 package db;
 
 import models.Advert;
+import models.Category;
 import models.Comment;
 import models.User;
 
@@ -13,10 +14,10 @@ public class Seeds {
         User user1 = new User("Bob Smith", "bobsmith1@hotmail.com");
         DBHelper.save(user1);
 
-        Advert advert1 = new Advert("Nokia 3310", "Classic Nokia indestructable mobile phone for sale", Category.TECHNOLOGY, 50.00, "Glasgow", user, "testphoto.imgur.com");
+        Advert advert1 = new Advert("Nokia 3310", "Classic Nokia indestructable mobile phone for sale", Category.TECHNOLOGY, 50.00, "Glasgow", user1, "testphoto.imgur.com");
         DBHelper.save(advert1);
 
-        Comment comment1 = new Comment("Question about product", "Does product have any changeable covers?", user, advert);
+        Comment comment1 = new Comment("Question about product", "Does product have any changeable covers?", user1, advert1);
         DBHelper.save(comment1);
     }
 }
